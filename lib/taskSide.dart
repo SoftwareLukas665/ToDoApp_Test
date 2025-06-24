363ab9fdf5cf208e3a7b7d86dd47cfbdd6aa710c
SoftwareLukas665 <luk.riethig@icloud.com>
2025-06-24 21:22:25 +0200

    UI Anpassungen

--- lib/taskSide.dart
+++ lib/taskSide.dart
@@ -10,40 +10,75 @@ class taskSide extends StatefulWidget {
 class _taskSideState extends State<taskSide> {
   @override
   Widget build(BuildContext context) {
+
+    //Alle Variablen der Klasse
+    final double screenWidth = MediaQuery.sizeOf(context).width;
+
+    //User-Interface
     return Scaffold(
       body: Column(
         children: [
+          AppBar(
+            forceMaterialTransparency: true, //Verhindert Farbänderung der AppBar beim scrollen
+            title: Text("To-Do", style:
+              TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.w700
+              ),
+            ), centerTitle: false, //backgroundColor: Colors.grey[250],
+          ),
+          
           Expanded(
               child: Container(
-                color: Colors.green,
-                width: MediaQuery.sizeOf(context).width,
+                width: screenWidth,
                 child: Column(
                   children: [
+                    Spacer(flex: 2),
+
+                    Padding(
+                        padding: EdgeInsetsGeometry.symmetric(horizontal: screenWidth*0.04),
+                      child: TextField(decoration:
+                        InputDecoration(
+                            hintText: "Aufgabe eingeben",
+                            labelText: "Test"
+                        ),
+                      ),
+                    ),
+                    
+
+
                     Spacer(),
 
-                    TextField(decoration: InputDecoration(hintText: "Aufgabe eingeben"),),
                     ElevatedButton(style: ElevatedButton.styleFrom(
                         backgroundColor: Colors.black,
-                        foregroundColor: Colors.white), onPressed: () {
-
-                    }, child: Text("Bestätigen")
+                        foregroundColor: Colors.white),
+                        onPressed: () {
+                          //Aktion eifügen
+                        }, child: Text("Bestätigen")
                     ),
 
-                    Spacer()
+                    Spacer(flex: 1)
                   ],
                 )
               )
           ),
 
+          Divider(height: 3, thickness: 3, color: Colors.black,),
+
           Expanded(
             flex: 2,
-              child: Container(
-                color: Colors.red,
-                width: MediaQuery.sizeOf(context).width,
-                child: Center(
-                  child: Text("Test"),
-                ),
-              )
+            child: Container(
+              width: screenWidth,
+              child: ListView.builder(itemCount: 20, itemBuilder: (context, index) {
+                //print(index.toString() + "x Objekt der Liste");
+
+                return ListTile(
+                  title: Text("Test" + index.toString())
+                );
+              },
+
+              ),
+            ),
           ),
         ],
       ),
